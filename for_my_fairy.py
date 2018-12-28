2d6f2a9c48346b2b51497cbf33d8977039d98068
Zixu Zhao <zhao@zixu.me>
2018-12-27 22:26:00 -0500

    Update for_my_fairy.py
    add an error handler and setup retry

--- for_my_fairy.py
+++ for_my_fairy.py
@@ -19,7 +19,7 @@ def forecast_to_string(forecast):
       min_temp = weather["main"]["temp_min"]
     if weather["main"]["temp_max"] > max_temp:
       max_temp = weather["main"]["temp_max"]
-    if "3h" in weather["rain"]:
+    if "rain" in weather and "3h" in weather["rain"]:
       date =  datetime.datetime.strptime(weather["dt_txt"], '%Y-%m-%d %H:%M:%S')
       hour = date.hour
       rain.append(u"{}点-{}点: 降雨量 {:5.2f}mm".format(hour + 5, hour + 8, weather["rain"]["3h"]))
@@ -27,46 +27,56 @@ def forecast_to_string(forecast):
     return u"未来12小时:\n最高气温: {}摄氏度 \n最低气温: {}摄氏度 \n无降雨".format(max_temp, min_temp)
   else:
     return u"未来12小时:\n最高气温: {}摄氏度 \n最低气温: {}摄氏度 \n可能降雨(mm: 毫米):\n{}".format(max_temp, min_temp, "\n".join(rain))
-
-def job():
+  
+def send(retry):
   # Here must setup an account on openweathermap.org to get private appid
   # The id here is the city id from openweathermap.org, you could search another city ids on its website
   # Here the 6176823 is waterloo
   waterloo_url = "http://api.openweathermap.org/data/2.5/weather?id=6176823&appid=*消音*&units=metric&lang=zh_cn"
-
-  waterloo = json.load(urlopen(waterloo_url))
-  
   # Here must setup an account on openweathermap.org to get private appid
   # The id here is the city id from openweathermap.org, you could search another city ids on its website
   # Here the 1796236 is ShangHai
   shanghai_url = "http://api.openweathermap.org/data/2.5/weather?id=1796236&appid=*消音*&units=metric&lang=zh_cn"
-
-  shanghai = json.load(urlopen(shanghai_url))
-
-  weather_waterloo = json_to_string(waterloo, u'滑铁卢')
-  weather_shanghai = json_to_string(shanghai, u'上海')
   
   # Here must setup an account on openweathermap.org to get private appid
   # The id here is the city id from openweathermap.org, you could search another city ids on its website
   # The interval is 3 hours each so cnt = 4 will be the weather stats for the next 12 hours
   shanghai_forecast_url = "http://api.openweathermap.org/data/2.5/forecast?id=1796236&appid=*消音*&units=metric&lang=zh_cn&cnt=4"
+  try: 
+    waterloo = json.load(urlopen(waterloo_url))
+    shanghai = json.load(urlopen(shanghai_url)) 
+    shanghai_forecast = json.load(urlopen(shanghai_forecast_url))
+  except HTTPError as e:
+    print('The server couldn\'t fulfill the request.')
+    print('Error code: ', e.code)
+    if retry > 0:
+      send(retry - 1)
+  except URLError as e:
+    print('We failed to reach a server.')
+    print('Reason: ', e.reason)
+    if retry > 0:
+      send(retry - 1)
+  else:
+    weather_waterloo = json_to_string(waterloo, u'滑铁卢')
+    weather_shanghai = json_to_string(shanghai, u'上海')
 
-  shanghai_forecast = json.load(urlopen(shanghai_forecast_url))
-  forecast_shanghai = forecast_to_string(shanghai_forecast["list"])
+    forecast_shanghai = forecast_to_string(shanghai_forecast["list"])
 
-  # Here must setup an unique alias for the friend who you will sent to
-  dear = itchat.search_friends(name='*消音*')
-  dear_username = dear[0]['UserName']
+    dear = itchat.search_friends(name='*消音*')
+    dear_username = dear[0]['UserName']
 
-  itchat.send(u"这里是*消音*天气广播\n\n以下是当前的天气情况:\n\n{}\n{}\n\n{}".format(weather_shanghai, forecast_shanghai, weather_waterloo), toUserName=dear_username)
+    itchat.send(u"这里是*消音*天气广播\n\n以下是当前的天气情况:\n\n{}\n{}\n\n{}".format(weather_shanghai, forecast_shanghai, weather_waterloo), toUserName=dear_username)
 
+def job():
+  send(5)
+  
 def init():
   # Here must setup an unique alias for the friend who you will sent to
   dear = itchat.search_friends(name='*消音*')
   dear_username = dear[0]['UserName']
 
   itchat.send(u"*消音*天气广播已经为宇宙第一可爱美丽善良的*消音*启动!\n每日更新时间为北京时间上午八点和晚上八点.\n希望*消音*开心.\n--机器人*消音*", toUserName=dear_username)
-
+  
 init()
 
 schedule.every().day.at("08:00").do(job)
